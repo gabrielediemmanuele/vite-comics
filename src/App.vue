c90ef493646eac4c3eb4ef454783a651eb5c36ef
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-08 16:08:58 +0200

    feat: create folder, all components, start to work

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Header_nav from "./components/Header_nav.vue";
+import Content_bar from "./components/Content_bar.vue";
+import Products_bar from "./components/Products_bar.vue";
+import Footer_Links from "./ccomponents/Footer_Links.vue";
+import Footer_socials from "./ccomponents/Footer_socials.vue";
+
+export default {
+  components: {
+    Header_nav,
+    Content_bar,
+    Products_bar,
+    Footer_Links,
+    Footer_socials,
+  },
+};
+</script>
+
+<template>
+  <h1>titolo</h1>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
